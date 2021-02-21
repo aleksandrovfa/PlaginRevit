d3c08b73d542f2b54b6e80faf3d40d348e26cb52
Fedor_Aleksandrov <aleksandrov.fa95@gmail.com>
2021-02-21 21:56:58 +0300

    1

--- PBGS_update_switchboard/UpdateSwitchboardStart.cs
+++ PBGS_update_switchboard/UpdateSwitchboardStart.cs
@@ -17,20 +17,36 @@ namespace PBGS_update_switchboard
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             Document doc = commandData.Application.ActiveUIDocument.Document;
-            FilteredElementCollector newSheetsFilter = new FilteredElementCollector(doc);
-            ICollection<Element> allsheets = newSheetsFilter.OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType().ToElements(); ;
+            FilteredElementCollector newFilter = new FilteredElementCollector(doc);
 
-            List<string> allSheetsNames = new List<string>();
+            /*получение всех листов ревита в качестве элементо,
+            далее через цикл создается list листов.
+            Потом проиходит формирование массива и сортировка с помощью массива
+            */
+            ICollection<Element> allsheets = newFilter.OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType().ToElements();
             List<ViewSheet> аllSheetsList = new List<ViewSheet>();
-
             foreach (Element sheetEl in allsheets)
             {
                 ViewSheet sheet = sheetEl as ViewSheet;
-                allSheetsNames.Add(sheet.Title);
                 аllSheetsList.Add(sheet);
             }
             ViewSheet[] allSheetsArray = аllSheetsList.ToArray();
             Array.Sort(allSheetsArray, new SheetsComparerByNum());
+
+            /*получение всех видов и что то бла бла 
+            */
+            //views = [];
+            //FilteredElementCollector collector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Views);
+            //views = collector.ToElements() as List<Autodesk.Revit.DB.View>;
+
+            FilteredElementCollector viewCollector = new FilteredElementCollector(doc);
+            viewCollector.OfClass(typeof(ViewDrafting));
+
+
+            IList<Element> allviews = newFilter.OfCategory(BuiltInCategory.OST_Views).ToElements(); ;
+
+
+
 
             UserControl userControl = new UserControl(allSheetsArray);
 
