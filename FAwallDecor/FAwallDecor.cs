041ee5bc92586c4bc4d40fc34675cb3d25484819
Fedor_Aleksandrov <aleksandrov.fa95@gmail.com>
2021-03-01 03:18:00 +0300

    Близок к концу

---
+++ FAwallDecor/FAwallDecor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+
+
+namespace FAwallDecor
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class FAwallDecor : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Получение объектов приложения и документа
+            UIApplication uiApp = commandData.Application;
+            Document doc = uiApp.ActiveUIDocument.Document;
+            Selection sel = uiApp.ActiveUIDocument.Selection;
+
+            RoomPickFilter roomPickFilter = new RoomPickFilter();
+            IList<Reference> rooms = sel.PickObjects(ObjectType.Element, roomPickFilter,"Выберите помещения для отделки");
+            //MessageBox.Show("збс");
+            foreach(Reference r in rooms)
+            {
+                Room room = doc.GetElement(r) as Room;
+                IList<IList<BoundarySegment>> loops = room.GetBoundarySegments(new SpatialElementBoundaryOptions());
+                foreach (IList<BoundarySegment> l in loops)
+                {
+                    foreach (BoundarySegment wallid in l)
+                    {
+                        Wall wall = doc.GetElement(wallid.ElementId) as Wall;
+
+
+                    }
+                }
+            }
+
+
+            return Result.Succeeded;
+        }
+    }
+    public class RoomPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element e)
+        {
+            return (e.Category.Id.IntegerValue.Equals(
+            (int)BuiltInCategory.OST_Rooms));
+        }
+        public bool AllowReference(Reference r, XYZ p)
+        {
+            return false;
+        }
+    }
+}
--- PBGS_update_switchboard/UpdateSwitchboardStart.cs
+++ PBGS_update_switchboard/UpdateSwitchboardStart.cs
@@ -68,12 +68,22 @@ namespace PBGS_update_switchboard
             {
                 View view = viewEl as View;
                 if (view.Name == allCheckedView[0].ToString())
-                {
-                    
+                {                    
                     mainView = view;
                     activeView.ActiveView = mainView;
                 }
                 
+            }
+
+            List<ViewSheet> mainSheets = new List<ViewSheet>();
+            foreach (Element sheetEl in allsheets)
+            {
+                ViewSheet sheet = sheetEl as ViewSheet;
+                if (allCheckedSheet.Contains(sheet.Title))
+                {
+                    mainSheets.Add(sheet);                    
+                }
+
             }
 
             /* Считывание всех аннотаций групп и создание главное словаря с аннотациями.
@@ -81,9 +91,67 @@ namespace PBGS_update_switchboard
              * обойтись словарем. Потом че нить напишу для сравнения параметров. День нахуй на это потратил, а получилось гавно)
             */
             ElementClassFilter filter = new ElementClassFilter(typeof(FamilyInstance));
+
             Dictionary<string, AnnotationSymbol> dictAnotMain = new Dictionary<string, AnnotationSymbol>();
+            dictAnotMain = GetAnnotationView(mainView, doc);
+          
+            Dictionary<string, Dictionary<string, AnnotationSymbol>> dictAnotMinor = 
+                new Dictionary<string,Dictionary<string, AnnotationSymbol>>();
+            foreach (ViewSheet sheet in mainSheets)
+            {
+                dictAnotMinor.Add(sheet.Title, GetAnnotationSheets(sheet, doc));
+            }
+            //************************************************************************
+            UIApplication uiApp = commandData.Application;
+            Selection sel = uiApp.ActiveUIDocument.Selection;
             
-            foreach (ElementId elid in mainView.GetDependentElements(filter).ToList()) 
+            foreach (var dictAnotminor in dictAnotMinor)
+            {
+                MessageBox.Show("Идет проверка по листу: "+ dictAnotminor.Key);
+                List<ElementId> ids = new List<ElementId>();
+                Dictionary<string, string> showGroup = new Dictionary<string, string>();
+                Transaction trans = new Transaction(doc);
+                trans.Start("Lab");
+
+
+                foreach (var dictAnot in dictAnotminor.Value)
+                {
+                    if (!dictAnotMain.Keys.Contains(dictAnot.Key))
+                    {
+                        showGroup.Add(dictAnot.Key, ": не найдена");
+                        ids.Add(dictAnot.Value.Id);                                                       
+                    }
+                    else
+                    {
+                        Dictionary<string,string> change = CompareAnnotations(dictAnotMain[dictAnot.Key],dictAnot.Value);
+                        if (change.Count > 0) showGroup.Add(dictAnot.Key, ": обновлено");
+                        else showGroup.Add(dictAnot.Key, ": всё норм");
+
+
+                    }                    
+                }
+                
+                MessageBox.Show(string.Join(Environment.NewLine, showGroup));
+                if (ids.Count > 0)
+                {
+                    XYZ point = sel.PickPoint("Укажите точку размещения групп щита");
+                    Group group = null;
+                    ICollection<ElementId> ids1 = ids;
+                    group = doc.Create.NewGroup(ids1);
+                    Group group1 = doc.Create.PlaceGroup(point, group.GroupType);
+                    group.UngroupMembers();
+                    group1.UngroupMembers();
+
+                }
+                trans.Commit();
+            }
+            return Result.Succeeded;
+        }
+        private Dictionary<string, AnnotationSymbol> GetAnnotationView(View view, Document doc)
+        {
+            ElementClassFilter filter = new ElementClassFilter(typeof(FamilyInstance));
+            Dictionary<string, AnnotationSymbol> dictAnot = new Dictionary<string, AnnotationSymbol>();
+            foreach (ElementId elid in view.GetDependentElements(filter).ToList())
             {
                 Element elsymbol = doc.GetElement(elid);
                 if (elsymbol.Category.Name == "Типовые аннотации")
@@ -96,90 +164,94 @@ namespace PBGS_update_switchboard
                         {
                             if (pr.Definition.Name == "Обозначение")
                             {
-                                dictAnotMain.Add(pr.AsString(), symbol);
+                                dictAnot.Add(pr.AsString(), symbol);
                             }
                         }
 
                     }
                 }
             }
-            Dictionary<string, Dictionary<string, AnnotationSymbol>> dictAnotMinor = 
-                new Dictionary<string,Dictionary<string, AnnotationSymbol>>();
-            foreach (Element sheetEl in allsheets)
+            return dictAnot;
+
+        }
+
+        private Dictionary<string, AnnotationSymbol> GetAnnotationSheets(ViewSheet view, Document doc)
+        {
+            ElementClassFilter filter = new ElementClassFilter(typeof(FamilyInstance));
+            Dictionary<string, AnnotationSymbol> dictAnot = new Dictionary<string, AnnotationSymbol>();
+            foreach (ElementId elid in view.GetDependentElements(filter).ToList())
             {
-                ViewSheet sheet = sheetEl as ViewSheet;
-                if (allCheckedSheet.Contains(sheet.Title))
+                Element elsymbol = doc.GetElement(elid);
+                if (elsymbol.Category.Name == "Типовые аннотации")
                 {
-                    //dictAnotMinor.Add(sheet.Title, new Dictionary<string, AnnotationSymbol>);
-                    foreach (ElementId elid in sheet.GetDependentElements(filter).ToList())
-                    {
-                        Element elsymbol = doc.GetElement(elid);
-                        if (elsymbol.Category.Name == "Типовые аннотации")
-                        {
-                            AnnotationSymbol symbol = elsymbol as AnnotationSymbol;
-                            if (symbol.AnnotationSymbolType.FamilyName == "# Reports - Схема - Силовой щит - Данные - ГРЩ" |
+                    AnnotationSymbol symbol = elsymbol as AnnotationSymbol;
+                    if (symbol.AnnotationSymbolType.FamilyName == "# Reports - Схема - Силовой щит - Данные - ГРЩ" |
                                 symbol.AnnotationSymbolType.FamilyName == "# Reports - Схема - Силовой щит - Данные")
+                    {
+                        foreach (Parameter pr in symbol.ParametersMap)
+                        {
+                            if (pr.Definition.Name == "Обозначение")
                             {
-                                foreach (Parameter pr in symbol.ParametersMap)
-                                {
-                                    if (pr.Definition.Name == "Обозначение")
-                                    {
-                                        if (dictAnotMinor.Keys.Contains(sheet.Title))
-                                        {
-                                            dictAnotMinor[sheet.Title].Add(pr.AsString(), symbol);
-                                        }
-                                        else
-                                        {
-                                            dictAnotMinor.Add(sheet.Title, new Dictionary<string, AnnotationSymbol> { { pr.AsString(), symbol } });
-                                        }
-                                    }
-                                }
+                                dictAnot.Add(pr.AsString(), symbol);
                             }
-
                         }
 
                     }
-
                 }
-
             }
+            return dictAnot;
+        }
 
-            UIApplication uiApp = commandData.Application;
-            Selection sel = uiApp.ActiveUIDocument.Selection;
-            foreach (var dictAnotminor in dictAnotMinor)
+        private Dictionary<string,string> CompareAnnotations (AnnotationSymbol A1, AnnotationSymbol A2)
+        {
+            Dictionary<String, String> change = new Dictionary<String, String>();
+            foreach (Parameter parameterА1 in A1.Parameters)
             {
-                MessageBox.Show("Идет проверка по листу: "+ dictAnotminor.Key);
-                List<ElementId> ids = new List<ElementId>();
-                Dictionary<string, string> showGroup = new Dictionary<string, string>();                
-                foreach (var dictAnot in dictAnotminor.Value)
+                if (parameterА1.HasValue & !parameterА1.IsReadOnly)
                 {
-                    if (!dictAnotMain.Keys.Contains(dictAnot.Key))
+                    if (parameterА1.StorageType == StorageType.Integer)
                     {
-                        showGroup.Add(dictAnot.Key, ": не найдена");
-                        ids.Add(dictAnot.Value.Id);                                                       
+                        string name1 =  parameterА1.Definition.Name;
+                        int value1 = parameterА1.AsInteger();
+                        IList<Parameter> parameterА2 = A2.GetParameters(name1);
+                        int value2 = parameterА2[0].AsInteger();
+                        if (value1 != value2)
+                        {
+                            change.Add(name1, "Изменено значения с " + value1 + " на " + value2);
+                            parameterА1.Set(value2);
+                        }
                     }
-                    else
+                    if (parameterА1.StorageType == StorageType.String)
                     {
-                        showGroup.Add(dictAnot.Key, ": всё норм");
-                        //MessageBox.Show("Всё норм найдено");                       
-                    }                    
-                }
-                MessageBox.Show(string.Join(Environment.NewLine, showGroup));
-                if (ids.Count > 0)
-                {
-                    XYZ point = sel.PickPoint("Укажите точку для размещения группы");
-                    Group group = null;
-                    Transaction trans = new Transaction(doc);
-                    trans.Start("Lab");
-                    ICollection<ElementId> ids1 = ids;
-                    group = doc.Create.NewGroup(ids1);
-                    Group group1 = doc.Create.PlaceGroup(point, group.GroupType);
-                    group.UngroupMembers();
-                    group1.UngroupMembers();
-                    trans.Commit();
+                        string name1 = parameterА1.Definition.Name;
+                        string value1 = parameterА1.AsString();
+                        ElementId id1 = parameterА1.Id;
+                        IList<Parameter> parameterА2 = A2.GetParameters(name1);
+                        string value2 = parameterА2[0].AsString();
+                        if (value1 != value2)
+                        {
+                            change.Add(name1, "Изменено значения с " + value1 + " на " + value2);
+                            parameterА1.Set(value2);
+                        }
+
+                    }
+                    if (parameterА1.StorageType == StorageType.Double)
+                    {
+                        string name1 = parameterА1.Definition.Name;
+                        double value1 = parameterА1.AsDouble();
+                        ElementId id1 = parameterА1.Id;
+                        IList<Parameter> parameterА2 = A2.GetParameters(name1);
+                        double value2 = parameterА2[0].AsDouble();
+                        if (value1 != value2)
+                        {
+                            change.Add(name1, "Изменено значения с " + value1 + " на " + value2);
+                            parameterА1.Set(value2);
+                        }
+
+                    }
                 }
             }
-            return Result.Succeeded;
+            return  change;
         }
     }
 }
